05f5f4ff6c77ce20e789cf23e2cf344fc31c3b8a
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-06-04 10:03:09 +0900

    ログイン機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
   end
 end
