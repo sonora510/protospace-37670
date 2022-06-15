cea40e8deb6d1cb0108824467f57de0adf34d56b
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-06-15 12:23:23 +0900

    コメント投稿機能の実装

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user #usersテーブルとのアソシエーション
   belongs_to :prototype #prototypeテーブルとのアソシエーショn
 
-  validates :text, presence: true
+  validates :content, presence: true
 end
