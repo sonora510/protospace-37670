0ddc307f8a9f32b36105ac9a5485c68f4d8a4cee
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-06-15 18:44:10 +0900

    自身のプロトタイプへ遷移できない問題を修正（空のコメント時）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,6 +21,7 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    binding.pry
     unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
